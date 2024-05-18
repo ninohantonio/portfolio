4c7ef2aa4c33c81e96b7cff6e902e494b4f91ba9
ninohantonio <ninohantonio@gmail.com>
2024-05-18 17:14:42 +0300

    fix header

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
 import {GiCurlyWing} from "react-icons/gi";
-import {IoMdHand} from "react-icons/io";
+import {IoMdArrowDropright, IoMdHand} from "react-icons/io";
 
 
 export default function HeroMain(){
@@ -10,17 +11,18 @@ export default function HeroMain(){
                 <div className={"text-blue-800 basis-[15%] text-[200px] mr-1 pt-10"}>
                     <GiCurlyWing/>
                 </div>
-                <div className="text-white flex flex-col items-center justify-center text-4xl font-bold mt-28 h-[60vh] mt-20 w-[40vw] pt-20 space-y-2 z-10 bg-gradient-to-r from-gray-700 to-transparent bg-opacity-20 pl-6 rounded-bl-2xl">
-                    <div className="">
-                        <h1 className="inline"> <span className="text-blue-500">Bienvenue</span> a vous cher</h1>
-                        <h1 className="text-yellow-300 inline"> Visiteur</h1>
-                        <span className="">,</span><br/>
-                    </div>
-                    <div>
-                        <h1 className="inline text-[#c4e4db]"><span className="text-[#3F51B5]">👋</span>Antonino Iraky Ny Avo</h1> 
-                    </div>
-                    <h1 className="inline">Developpeur et concepteur de systeme d&apos; informations</h1>
-                   
+                <div className="text-white flex flex-col items-center justify-start text-4xl font-bold mt-28 h-[60vh] mt-20 w-[40vw] py-10 px-6 space-y-8 z-10 bg-gradient-to-r from-gray-700 to-transparent bg-opacity-20 rounded-bl-2xl">
+                   <h1>
+                       Hey 👋. I&apos;m <span className="text-blue-500">Antonino Iraky Ny Avo. </span>Developer and Information Systems Designer
+                   </h1>
+                   <p className="font-mono text-lg left-0">
+                     I&apos;m here to realize your digital idea 🤗
+                   </p>
+                   <div className="w-full flex justify-start">
+                        <div className="border-2 px-6  py-2 text-lg flex flex-row items-center space-x-4">
+                            <span>Hire me</span> <FaArrowRight/>
+                        </div>
+                   </div>
                 </div>
                 <div className="w-[40vw] h-[30vh] -z-2 bg-white bg-gradient-conic from-gray-700 to-blue-900 blur-lg center-absolute-hero">
 
@@ -29,8 +31,8 @@ export default function HeroMain(){
                     👋🏿
                 </div> */}
             </div>
-            <div className="ml-20 px-10 bg-gradient-to-t from-gray-700 to-transparent mt-20 rounded-b-xl">
-                <Image src={"/NinoPhoto.png"} alt={"antoninoPhoto"} width={250} height={250}/>
+            <div className="ml-20 px-10 from-gray-700 to-transparent rounded-b-xl">
+                <Image src={"/NinoPhoto.png"} alt={"antoninoPhoto"} width={300} height={300}/>
             </div>
         </div>
     )
