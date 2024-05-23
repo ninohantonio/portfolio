b0658cc8d1b72c9ec4089de800bd27427cdef440
ninohantonio <ninohantonio@gmail.com>
2024-05-23 15:05:58 +0300

    update smoother

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smoother">
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
           <Cursor/>
          {children}
